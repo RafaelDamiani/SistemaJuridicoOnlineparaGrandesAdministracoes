77efbb2e4ea23f022a543a135a1f3f7decc0c801
RafaelDamiani <rafael2605@gmail.com>
2020-06-20 15:50:58 -0300

    Changed Java project to Web Application with Glassfish server

---
+++ src/java/model/User.java
@@ -0,0 +1,38 @@
+package model;
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.SequenceGenerator;
+
+@Entity
+@Table(name = "tb_user")
+@SequenceGenerator(name = "sequencia", sequenceName = "seq_id")
+public class User {
+
+    private Long id;
+    private String name;
+
+    public User() {
+    }
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "sequencia")
+    public Long getId() {
+        return this.id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public void setName(String nome) {
+        this.name = nome;
+    }
+}
