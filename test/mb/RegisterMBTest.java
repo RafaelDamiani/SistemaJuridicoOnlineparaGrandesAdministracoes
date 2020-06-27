530162198f8f50efee08097c2e3c526bd63202ef
RafaelDamiani <rafael2605@gmail.com>
2020-06-27 19:06:20 -0300

    RegisterMBTest Added

---
+++ test/mb/RegisterMBTest.java
@@ -0,0 +1,315 @@
+package mb;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class RegisterMBTest {
+    
+    public RegisterMBTest() {
+    }
+    
+    @BeforeClass
+    public static void setUpClass() {
+    }
+    
+    @AfterClass
+    public static void tearDownClass() {
+    }
+    
+    @Before
+    public void setUp() {
+    }
+    
+    @After
+    public void tearDown() {
+    }
+
+    /**
+     * Test of getEmail method, of class RegisterMB.
+     */
+    @Test
+    public void testGetEmail() {
+        System.out.println("getEmail");
+        RegisterMB instance = new RegisterMB();
+        String expResult = "";
+        String result = instance.getEmail();
+        assertEquals(expResult, result);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of setEmail method, of class RegisterMB.
+     */
+    @Test
+    public void testSetEmail() {
+        System.out.println("setEmail");
+        String email = "";
+        RegisterMB instance = new RegisterMB();
+        instance.setEmail(email);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of getPassword method, of class RegisterMB.
+     */
+    @Test
+    public void testGetPassword() {
+        System.out.println("getPassword");
+        RegisterMB instance = new RegisterMB();
+        String expResult = "";
+        String result = instance.getPassword();
+        assertEquals(expResult, result);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of setPassword method, of class RegisterMB.
+     */
+    @Test
+    public void testSetPassword() {
+        System.out.println("setPassword");
+        String password = "";
+        RegisterMB instance = new RegisterMB();
+        instance.setPassword(password);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of getName method, of class RegisterMB.
+     */
+    @Test
+    public void testGetName() {
+        System.out.println("getName");
+        RegisterMB instance = new RegisterMB();
+        String expResult = "";
+        String result = instance.getName();
+        assertEquals(expResult, result);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of setName method, of class RegisterMB.
+     */
+    @Test
+    public void testSetName() {
+        System.out.println("setName");
+        String name = "";
+        RegisterMB instance = new RegisterMB();
+        instance.setName(name);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of getCpf method, of class RegisterMB.
+     */
+    @Test
+    public void testGetCpf() {
+        System.out.println("getCpf");
+        RegisterMB instance = new RegisterMB();
+        String expResult = "";
+        String result = instance.getCpf();
+        assertEquals(expResult, result);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of setCpf method, of class RegisterMB.
+     */
+    @Test
+    public void testSetCpf() {
+        System.out.println("setCpf");
+        String cpf = "";
+        RegisterMB instance = new RegisterMB();
+        instance.setCpf(cpf);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of getIdUserType method, of class RegisterMB.
+     */
+    @Test
+    public void testGetIdUserType() {
+        System.out.println("getIdUserType");
+        RegisterMB instance = new RegisterMB();
+        Integer expResult = null;
+        Integer result = instance.getIdUserType();
+        assertEquals(expResult, result);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of setIdUserType method, of class RegisterMB.
+     */
+    @Test
+    public void testSetIdUserType() {
+        System.out.println("setIdUserType");
+        Integer idUserType = null;
+        RegisterMB instance = new RegisterMB();
+        instance.setIdUserType(idUserType);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of getZipCode method, of class RegisterMB.
+     */
+    @Test
+    public void testGetZipCode() {
+        System.out.println("getZipCode");
+        RegisterMB instance = new RegisterMB();
+        String expResult = "";
+        String result = instance.getZipCode();
+        assertEquals(expResult, result);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of setZipCode method, of class RegisterMB.
+     */
+    @Test
+    public void testSetZipCode() {
+        System.out.println("setZipCode");
+        String zipCode = "";
+        RegisterMB instance = new RegisterMB();
+        instance.setZipCode(zipCode);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of getStreet method, of class RegisterMB.
+     */
+    @Test
+    public void testGetStreet() {
+        System.out.println("getStreet");
+        RegisterMB instance = new RegisterMB();
+        String expResult = "";
+        String result = instance.getStreet();
+        assertEquals(expResult, result);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of setStreet method, of class RegisterMB.
+     */
+    @Test
+    public void testSetStreet() {
+        System.out.println("setStreet");
+        String street = "";
+        RegisterMB instance = new RegisterMB();
+        instance.setStreet(street);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of getNumber method, of class RegisterMB.
+     */
+    @Test
+    public void testGetNumber() {
+        System.out.println("getNumber");
+        RegisterMB instance = new RegisterMB();
+        Short expResult = null;
+        Short result = instance.getNumber();
+        assertEquals(expResult, result);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of setNumber method, of class RegisterMB.
+     */
+    @Test
+    public void testSetNumber() {
+        System.out.println("setNumber");
+        Short number = null;
+        RegisterMB instance = new RegisterMB();
+        instance.setNumber(number);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of getCity method, of class RegisterMB.
+     */
+    @Test
+    public void testGetCity() {
+        System.out.println("getCity");
+        RegisterMB instance = new RegisterMB();
+        String expResult = "";
+        String result = instance.getCity();
+        assertEquals(expResult, result);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of setCity method, of class RegisterMB.
+     */
+    @Test
+    public void testSetCity() {
+        System.out.println("setCity");
+        String city = "";
+        RegisterMB instance = new RegisterMB();
+        instance.setCity(city);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of getState method, of class RegisterMB.
+     */
+    @Test
+    public void testGetState() {
+        System.out.println("getState");
+        RegisterMB instance = new RegisterMB();
+        String expResult = "";
+        String result = instance.getState();
+        assertEquals(expResult, result);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of setState method, of class RegisterMB.
+     */
+    @Test
+    public void testSetState() {
+        System.out.println("setState");
+        String state = "";
+        RegisterMB instance = new RegisterMB();
+        instance.setState(state);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of registeruser method, of class RegisterMB.
+     */
+    @Test
+    public void testRegisteruser() {
+        System.out.println("registeruser");
+        RegisterMB instance = new RegisterMB();
+        String expResult = "";
+        String result = instance.registeruser();
+        assertEquals(expResult, result);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+    
+}
